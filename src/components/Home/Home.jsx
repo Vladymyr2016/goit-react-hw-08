b9bc127ee15a945937e78d728453ebaff8661e47
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-05-22 13:57:31 +0200

    img home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,11 @@
 import s from './Home.module.css';
 
 const Home = () => {
-  return <div className={s.home}>{}</div>;
+  return (
+    <div className={s.home}>
+      <img src="https://cdn.pixabay.com/video/2023/07/06/170308-842905573_tiny.jpg" />
+    </div>
+  );
 };
 
 export default Home;
